ddc2144a6c17dbaaf5e94b25adee9a9f7894244e
Joshua Wang <wangjoshua8@gmail.com>
2024-05-17 22:07:08 +1000

    Add in Adamic-Adar index link prediction.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from load_data import load_graph
+from utilities import *
+from neighbourhood_prediction import adamic_adar_index
 
 
 if __name__ == "__main__":
     training_file = "./data/trainingset.csv"
+    test_file = "./data/testset.csv"
 
     train_graph = load_graph(training_file)
+    test_list = load_test_set_as_list(test_file)
 
-    print(train_graph.number_of_edges())
+    # Adamic-Adar Index Scoring
+    aa_index = adamic_adar_index(train_graph, test_list)
+    write_top_links(find_top_links(test_list, aa_index))
+    write_full_results(label_top_links(test_list, aa_index))
